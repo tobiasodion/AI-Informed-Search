669d120d6fd8ab0516b2b75f6dbbd49856d65767
tobias odion <tobiasodion@tobiass-MacBook-Pro.local>
2022-04-28 02:51:54 +0200

    completed A* search algorithm

---
+++ Question_3_A_star_search.py
@@ -0,0 +1,114 @@
+#Question 3 - PART A
+from collections import deque
+ 
+class Graph:
+    def __init__(self, graph, node_heuristics):
+        self.adjac_lis = graph
+        self.heuristics = node_heuristics
+ 
+    def get_neighbors(self, v):
+        return self.adjac_lis[v]
+ 
+    # This is heuristic function which is having equal values for all nodes
+    def h(self, n):
+        H = self.heuristics
+        return H[n]
+ 
+    def a_star_search(self, start, stop):
+        # In this open_lst is a list of nodes which have been visited, but who's 
+        # neighbours haven't all been always inspected, It starts off with the start 
+        #node
+        # And closed_lst is a list of nodes which have been visited
+        # and who's neighbors have been always inspected
+        open_lst = set([start])
+        closed_lst = set([])
+ 
+        # poo has present distances from start to all other nodes
+        # the default value is +infinity
+        poo = {}
+        poo[start] = 0
+ 
+        # par contains an adjac mapping of all nodes
+        par = {}
+        par[start] = start
+ 
+        while len(open_lst) > 0:
+            n = None
+            print('Fringe: {}'.format(poo))
+            # it will find a node with the lowest value of f() -
+            for v in open_lst:
+                if n == None or poo[v] + self.h(v) < poo[n] + self.h(n):
+                    n = v;
+ 
+            if n == None:
+                print('Path does not exist!')
+                return None
+
+            
+            # if the current node is the stop
+            # then we start again from start
+            if n == stop:
+                reconst_path = []
+
+                while par[n] != n:
+                    reconst_path.append(n)
+                    n = par[n]
+ 
+                reconst_path.append(start)
+ 
+                reconst_path.reverse()
+ 
+                print('Path found: {}'.format(reconst_path))
+                return reconst_path
+ 
+            # for all the neighbors of the current node do
+            for (m, weight) in self.get_neighbors(n):
+              # if the current node is not presentin both open_lst and closed_lst
+                # add it to open_lst and note n as it's par
+                if m not in open_lst and m not in closed_lst:
+                    open_lst.add(m)
+                    par[m] = n
+                    poo[m] = poo[n] + weight
+ 
+                # otherwise, check if it's quicker to first visit n, then m
+                # and if it is, update par data and poo data
+                # and if the node was in the closed_lst, move it to open_lst
+                else:
+                    if poo[m] > poo[n] + weight:
+                        poo[m] = poo[n] + weight
+                        par[m] = n
+ 
+                        if m in closed_lst:
+                            closed_lst.remove(m)
+                            open_lst.add(m)
+ 
+            # remove n from the open_lst, and add it to closed_lst
+            # because all of his neighbors were inspected
+            open_lst.remove(n)
+            closed_lst.add(n)
+ 
+        print('Path does not exist!')
+        return None
+
+#Question 3 - PART B
+#define graph
+graph = {
+    'S': [('A', 2), ('B', 3), ('D', 5)],
+    'A': [('C', 4)],
+    'B': [('D', 4)],
+    'C': [('D', 1), ('G',2)],
+    'D': [('C', 1), ('G',5)]
+}
+
+#define heuristics
+node_heuristics =  {
+            'S': 0,
+            'A': 2,
+            'B': 5,
+            'C': 2,
+            'D': 1,
+            'G': 0
+        }
+
+graph1 = Graph(graph,node_heuristics)
+graph1.a_star_search('S', 'G')
